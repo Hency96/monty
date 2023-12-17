01d993e0f6d02f838369b50edc8a4f16033f7f8e
Monday <hencynikky@gmail.com>
2023-12-17 14:25:52 -0800

    fix for div

--- divs.c
+++ divs.c
@@ -8,7 +8,7 @@
 void f_div(stack_t **head, unsigned int counter)
 {
 	stack_t *h;
-	int len = 0, aux;
+	int len = 0, au;
 
 	h = *head;
 	while (h)
@@ -25,7 +25,7 @@ void f_div(stack_t **head, unsigned int counter)
 		exit(EXIT_FAILURE);
 	}
 	h = *head;
-	if (h->n == 0)
+	if (h->s == 0)
 	{
 		fprintf(stderr, "L%d: division by zero\n", counter);
 		fclose(bus.file);
@@ -33,8 +33,8 @@ void f_div(stack_t **head, unsigned int counter)
 		free_stack(*head);
 		exit(EXIT_FAILURE);
 	}
-	aux = h->next->n / h->n;
-	h->next->n = aux;
+	au = h->next->s / h->s;
+	h->next->s = au;
 	*head = h->next;
 	free(h);
 }
