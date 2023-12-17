b3f1dda6f2134627eb30d6ae7cccbfc7d9bcb85c
Monday <hencynikky@gmail.com>
2023-12-17 15:31:46 -0800

    fix for mod

--- mods.c
+++ mods.c
@@ -9,15 +9,15 @@
 void f_mod(stack_t **head, unsigned int counter)
 {
 	stack_t *h;
-	int len = 0, aux;
+	int size = 0, a;
 
 	h = *head;
 	while (h)
 	{
 		h = h->next;
-		len++;
+		size++;
 	}
-	if (len < 2)
+	if (size < 2)
 	{
 		fprintf(stderr, "L%d: can't mod, stack too short\n", counter);
 		fclose(bus.file);
@@ -34,8 +34,8 @@ void f_mod(stack_t **head, unsigned int counter)
 		free_stack(*head);
 		exit(EXIT_FAILURE);
 	}
-	aux = h->next->n % h->n;
-	h->next->n = aux;
+	a = h->next->n % h->n;
+	h->next->n = a;
 	*head = h->next;
 	free(h);
 }
